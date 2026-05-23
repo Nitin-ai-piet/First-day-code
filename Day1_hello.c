9c8f9e7afd9325d62d09774d84de15984b773e23
Nitin-ai-piet <nitinsharma2632008@gmail.com>
2026-05-23 22:33:16 +0530

    Day1_hello.c

---
+++ Day1_hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello Duniya! Main Nitin hu PIET se 👋\n");
+    printf("Day 1 Complete - GitHub + C Code Done ✅\n");
+    printf("Laptop nahi hai to kya hua, phone hi kaafi hai 💪\n");
+    printf("Roz thoda thoda, 30 din me C ka Pro 📈\n");
+    printf("Coding journey shuru ho gayi 🚀\n");
+    return 0;
+}
