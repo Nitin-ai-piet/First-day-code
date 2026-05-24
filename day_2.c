dc9f7ac9d67e3f22e240836d74ba2fefee1027ef
Nitin-ai-piet <nitinsharma2632008@gmail.com>
2026-05-24 08:06:19 +0530

    Add files via upload

---
+++ day_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+
+    printf("Pehla number: %d\n", a);
+    printf("Dusra number: %d\n", b);
+    printf("Dono ka Total: %d\n", sum);
+
+    return 0;
+}
